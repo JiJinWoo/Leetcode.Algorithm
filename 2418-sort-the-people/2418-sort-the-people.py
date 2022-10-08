3427899f54f0e8fa4c1461acabe7294c04826140
Earthquakoo <97788459+JiJinWoo@users.noreply.github.com>
2022-10-08 15:59:25 +0900

    Time: 495 ms (7.81%), Space: 14.4 MB (89.42%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,16 +1,12 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         
-        hash_map = {}
         result = []
 
+        sort_heights = sorted(heights, reverse=True)
 
-        for i in range(len(names)):
-            hash_map[heights[i]] = names[i]
-
-        sort_people = sorted(hash_map.items(), reverse=True)
-
-        for i in range(len(names)):
-            result.append(sort_people[i][1])
+        for i in sort_heights:
+            index = heights.index(i)
+            result.append(names[index])
             
         return result
