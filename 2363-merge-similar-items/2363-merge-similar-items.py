e15a30c6de500dd100822fefc158675795f32d3c
Earthquakoo <97788459+JiJinWoo@users.noreply.github.com>
2022-10-19 19:42:57 +0900

    Time: 188 ms (73.02%), Space: 14.7 MB (83.96%) - LeetHub

--- 2363-merge-similar-items/2363-merge-similar-items.py
+++ 2363-merge-similar-items/2363-merge-similar-items.py
@@ -1,16 +1,8 @@
 class Solution:
     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
-        items_dct = {}
+        items_dct = defaultdict(int)
 
-        items = items1 + items2
+        for l in items1+items2:
+            items_dct[l[0]] += l[1]
 
-        for l in items:
-            if l[0] not in items_dct:
-                items_dct[l[0]] = l[1]
-            else:
-                items_dct[l[0]] += l[1]
-
-
-        result = sorted(items_dct.items())
-        
-        return result
+        return sorted(items_dct.items())
