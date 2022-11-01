da0017579c9127059d242942cecf270e721fa0d1
Earthquakoo <97788459+JiJinWoo@users.noreply.github.com>
2022-11-01 23:29:43 +0900

    Time: 74 ms (91.19%), Space: 14.3 MB (62.17%) - LeetHub

---
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.py
@@ -0,0 +1,17 @@
+class Solution:
+    def intersection(self, nums: List[List[int]]) -> List[int]:
+        dct = defaultdict(int)
+
+        arr = []
+
+        for num in nums:
+            for n in num:
+                dct[n] += 1
+
+        for item in dct.items():
+            if (item[1] % len(nums) == 0):
+                arr.append(item[0])
+        
+        res = sorted(arr)
+        
+        return res
